8ac68ebf29b9fcd3cf11b650ff5c27d3ab7a2055
ihor <ihprishchepa@gmail.com>
2023-07-02 21:19:59 +0300

    sec com

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        /*
-        int a = 0, x = 5;
-        double b = 7;
-        System.out.println("Hello " + "World");
-        System.out.println(5);
-        System.out.println(5+5);
-        String name = "John";
-        x = 5;
-        int myVal;
-        myVal = 10;
-        float y = 5.6f;
-        System.out.println(name);
-        System.out.println(x);
-        System.out.println(myVal);
-        System.out.println(a+b);
-
-
-
-        double myDouble = 9.0;
-        int myInt = (int) myDouble;
-
-        System.out.println(myDouble);
-        System.out.println(myInt);
-
-         */
+        String name = "Hello ";
         int num = 1;
         double data = 1.0;
         int c = (int)data;
         num = c;
-        System.out.println(num);
+        System.out.println(name + "World");
         System.out.println(data);
         System.out.println(c);
     }
