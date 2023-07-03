37a7b78c38c36e8ab3fb14bc6de575e6c63adfab
ihor <ihprishchepa@gmail.com>
2023-07-03 11:12:49 +0300

    add scanner

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String name = "Hello ";
-        int num = 1;
-        double data = 1.0;
-        int c = (int)data;
-        num = c;
-        System.out.println(name + "World");
-        System.out.println(data);
-        System.out.println(c);
+        int a = 10, b = 6;
+        System.out.println(a%b);
     }
 }
---
+++ src/Scaner.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Scaner {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = sc.nextLine();
+        System.out.println("Where are you live, " + name + " ?");
+        String address = sc.nextLine();
+        System.out.println("I live in " + address + " city");
+    }
+}
